af32516e6f434503bbbda7ddb73ad479b46f45fd
Peter Malo <peter.malo@centrum.sk>
2017-11-29 20:28:37 +0100

    lab5 asi ide

--- inc/lib.h
+++ inc/lib.h
@@ -50,10 +50,9 @@ int	sys_env_destroy(envid_t);
 void	sys_yield(void);
 static envid_t sys_exofork(void);
 int	sys_env_set_status(envid_t env, int status);
-<<<<<<< HEAD
+
 int	sys_env_set_trapframe(envid_t env, struct Trapframe *tf);
-=======
->>>>>>> origin/lab4
+
 int	sys_env_set_pgfault_upcall(envid_t env, void *upcall);
 int	sys_page_alloc(envid_t env, void *pg, int perm);
 int	sys_page_map(envid_t src_env, void *src_pg,
@@ -83,7 +82,6 @@ envid_t	ipc_find_env(enum EnvType type);
 envid_t	fork(void);
 envid_t	sfork(void);	// Challenge!
 
-<<<<<<< HEAD
 // fd.c
 int	close(int fd);
 ssize_t	read(int fd, void *buf, size_t nbytes);
@@ -121,9 +119,7 @@ int	pipeisclosed(int pipefd);
 
 // wait.c
 void	wait(envid_t env);
-=======
 
->>>>>>> origin/lab4
 
 /* File open modes */
 #define	O_RDONLY	0x0000		/* open for reading only */
